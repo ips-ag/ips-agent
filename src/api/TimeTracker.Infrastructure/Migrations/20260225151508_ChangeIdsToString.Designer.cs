86d74da749f9a96886f50525875b3d5393de16f7
Uroš Miletić <uros.miletic@ips-ag.com>
2026-02-25 16:20:44 +0100

    Align database structure with db context

--- src/api/TimeTracker.Infrastructure/Migrations/20260225151508_ChangeIdsToString.Designer.cs
+++ src/api/TimeTracker.Infrastructure/Migrations/20260225151508_ChangeIdsToString.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimeTracker.Infrastructure.Data;
 
@@ -10,9 +11,11 @@ using TimeTracker.Infrastructure.Data;
 namespace TimeTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(TimeTrackerDbContext))]
-    partial class TimeTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225151508_ChangeIdsToString")]
+    partial class ChangeIdsToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
